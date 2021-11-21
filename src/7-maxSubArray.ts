533591b4183b55e3567d5a490eee421a66f8d8e7
amanovofficial <amanovofficial@yandex.ru>
2021-11-21 23:41:59 +0500

    7 - max sub array

---
+++ src/7-maxSubArray.ts
@@ -0,0 +1,15 @@
+function maxSubArray(nums: number[]): number {
+
+    let maxSum = nums[0];
+    let currentSum = nums[0]
+    for (let i = 1; i < nums.length; i++) {
+        currentSum = Math.max(currentSum + nums[i], nums[i])
+        maxSum = Math.max(maxSum, currentSum);
+    }
+
+    return maxSum;
+};
+
+
+
+console.log(maxSubArray([-2,1, -3,4, -1,2,1, -5,4]));
